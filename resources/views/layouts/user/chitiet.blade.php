412c91a91611af9f97cfb8c5ef99325fba17435b
VoQuangBao <0306221104@caothang.edu.vn>
2025-01-17 10:19:13 +0700

    Thêm sản phẩm liên quan

--- resources/views/layouts/user/chitiet.blade.php
+++ resources/views/layouts/user/chitiet.blade.php
@@ -7,23 +7,24 @@
                 <li><a href="{{ url('/') }}">Home</a> <span class="divider">/</span></li>
                 <li><a href="{{ url('/product') }}">Sản phẩm</a> <span class="divider">/</span></li>
                 <li class="active">
-                @isset($chitietsp)
-                    {{ $chitietsp->TenSach }}
-                @else
-                    Sản phẩm không tồn tại
-                @endisset</li>
+                    @isset($chitietsp)
+                        {{ $chitietsp->TenSach }}
+                    @else
+                        Sản phẩm không tồn tại
+                    @endisset
+                </li>
             </ul>
             <div class="row">
                 <div id="gallery" class="span3">
-                    @if(isset($chitietsp) && $chitietsp != null)
-                    <a href="{{ asset('uploads/books/' . $chitietsp->HinhAnh) }}" title="{{ $chitietsp->TenSach }}">
-                        <img src="{{ asset('uploads/books/' . $chitietsp->HinhAnh) }}" style="width:100%" alt="{{ $chitietsp->TenSach }}" />
-                    </a>
-                @else
-                <h3>{{ isset($chitietsp) ? $chitietsp->TenSach : 'Sản phẩm không tồn tại' }}</h3>
+                    @if (isset($chitietsp) && $chitietsp != null)
+                        <a href="{{ asset('uploads/books/' . $chitietsp->HinhAnh) }}" title="{{ $chitietsp->TenSach }}">
+                            <img src="{{ asset('uploads/books/' . $chitietsp->HinhAnh) }}" style="width:100%"
+                                alt="{{ $chitietsp->TenSach }}" />
+                        </a>
+                    @else
+                        <h3>{{ isset($chitietsp) ? $chitietsp->TenSach : 'Sản phẩm không tồn tại' }}</h3>
+                    @endif
 
-                @endif
-                    
                     </a>
                     <div id="differentview" class="moreOptopm carousel slide">
                         <div class="carousel-inner">
@@ -50,44 +51,46 @@
                     <hr class="soft" />
                     <form class="form-horizontal qtyFrm" action="{{ route('cart.add') }}" method="POST">
                         @csrf
-                        
+
                         <input type="hidden" name="product_id" value="{{ $chitietsp->MaSach }}"> <!-- id -->
                         <input type="hidden" name="price" value="{{ $chitietsp->GiaBan }}">
                         <div class="control-group">
                             <div class="controls">
-                                <div style="display: flex; align-items: center; justify-content: space-between; width: 150px">
+                                <div
+                                    style="display: flex; align-items: center; justify-content: space-between; width: 150px">
                                     <b>Số lượng</b>
-                                    <input type="number" class="span1" name="quantity" placeholder="Qty." value="1" min="1" />
+                                    <input type="number" class="span1" name="quantity" placeholder="Qty." value="1"
+                                        min="1" />
                                 </div>
-                    
+
                                 <button type="submit" class="btn btn-large btn-primary pull-right">Thêm vào giỏ hàng <i
                                         class="icon-shopping-cart"></i></button>
                             </div>
                         </div>
                     </form>
-                    
-                    @if(session('success'))
-                    <div class="alert alert-success">
-                        {{ session('success') }}
-                    </div>
-                @endif
-                
-                @if(session('error'))
-                    <div class="alert alert-danger">
-                        {{ session('error') }}
-                    </div>
-                @endif
+
+                    @if (session('success'))
+                        <div class="alert alert-success">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+
+                    @if (session('error'))
+                        <div class="alert alert-danger">
+                            {{ session('error') }}
+                        </div>
+                    @endif
                     <hr class="soft" />
                     <h4 id="product-quantity">Còn: {{ $chitietsp->SoLuong }} quyển</h4>
                     <script>
                         function updateProductQuantity() {
-                            var ProductSlug = '{{ $chitietsp->slug }}'; 
+                            var ProductSlug = '{{ $chitietsp->slug }}';
                             $.ajax({
-                                url: '/chitietsp/' + ProductSlug + '/soluong',  
-                                type: 'GET',
-                                success: function(response) {
-                                    if (response.SoLuong !== undefined) 
-                                        $('#product-quantity').text('Còn: ' + response.SoLuong + ' quyển');  
+                                    url: '/chitietsp/' + ProductSlug + '/soluong',
+                                    type: 'GET',
+                                    success: function(response) {
+                                        if (response.SoLuong !== undefined)
+                                            $('#product-quantity').text('Còn: ' + response.SoLuong + ' quyển');
                                     } else {
                                         alert('Không lấy được số lượng sản phẩm');
                                     }
@@ -97,12 +100,12 @@
                                 }
                             });
                         }
-                    
+
                         // Gọi hàm mỗi 5 giây để cập nhật số lượng
                         setInterval(updateProductQuantity, 5000);
                     </script>
-                    
-                    
+
+
                     <hr class="soft clr" />
                     @php
                         $text = $chitietsp->MoTa; // Lấy nội dung từ cơ sở dữ liệu
@@ -143,115 +146,25 @@
                                     </tr>
                                 </tbody>
                             </table>
-
-
                         </div>
                         <div class="tab-pane fade" id="profile">
                             <div class="tab-content">
                                 <div class="tab-pane active" id="blockView">
                                     <ul class="thumbnails">
-                                        <li class="span3">
-                                            <div class="thumbnail">
-                                                <a href="product_details.html"><img src="themes/images/products/10.jpg"
-                                                        alt="" /></a>
-                                                <div class="caption">
-                                                    <h5>Manicure &amp; Pedicure</h5>
-                                                    <p>
-                                                        Lorem Ipsum is simply dummy text.
-                                                    </p>
-                                                    <h4 style="text-align:center"><a class="btn"
-                                                            href="product_details.html"> <i class="icon-zoom-in"></i></a>
-                                                        <a class="btn" href="#">Add to <i
-                                                                class="icon-shopping-cart"></i></a> <a
-                                                            class="btn btn-primary" href="#">&euro;222.00</a></h4>
+                                        @foreach ($sachlienquan as $sachlq)
+                                            <li class="span3">
+                                                <div class="thumbnail">
+                                                    <a href="{{ route('chitietsp.index', $sachlq->slug) }}">
+                                                        <img src="{{ asset('uploads/books/' . $sachlq->HinhAnh) }}"
+                                                            alt="" /></a>
+                                                    <div class="caption">
+                                                        <h5>{{ $sachlq->TenSach }}</h5>
+                                                        <a class="btn" href="{{ url('/chitietsp/'.$sachlq->slug)}}">Xem chi tiết</a> 
+                                                        <a class="btn btn-primary" href="#">{{ $sachlq->GiaBan }} VND</a>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </li>
-                                        <li class="span3">
-                                            <div class="thumbnail">
-                                                <a href="product_details.html"><img src="themes/images/products/11.jpg"
-                                                        alt="" /></a>
-                                                <div class="caption">
-                                                    <h5>Manicure &amp; Pedicure</h5>
-                                                    <p>
-                                                        Lorem Ipsum is simply dummy text.
-                                                    </p>
-                                                    <h4 style="text-align:center"><a class="btn"
-                                                            href="product_details.html"> <i class="icon-zoom-in"></i></a>
-                                                        <a class="btn" href="#">Add to <i
-                                                                class="icon-shopping-cart"></i></a> <a
-                                                            class="btn btn-primary" href="#">&euro;222.00</a></h4>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li class="span3">
-                                            <div class="thumbnail">
-                                                <a href="product_details.html"><img src="themes/images/products/12.jpg"
-                                                        alt="" /></a>
-                                                <div class="caption">
-                                                    <h5>Manicure &amp; Pedicure</h5>
-                                                    <p>
-                                                        Lorem Ipsum is simply dummy text.
-                                                    </p>
-                                                    <h4 style="text-align:center"><a class="btn"
-                                                            href="product_details.html"> <i class="icon-zoom-in"></i></a>
-                                                        <a class="btn" href="#">Add to <i
-                                                                class="icon-shopping-cart"></i></a> <a
-                                                            class="btn btn-primary" href="#">&euro;222.00</a></h4>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li class="span3">
-                                            <div class="thumbnail">
-                                                <a href="product_details.html"><img src="themes/images/products/13.jpg"
-                                                        alt="" /></a>
-                                                <div class="caption">
-                                                    <h5>Manicure &amp; Pedicure</h5>
-                                                    <p>
-                                                        Lorem Ipsum is simply dummy text.
-                                                    </p>
-                                                    <h4 style="text-align:center"><a class="btn"
-                                                            href="product_details.html"> <i class="icon-zoom-in"></i></a>
-                                                        <a class="btn" href="#">Add to <i
-                                                                class="icon-shopping-cart"></i></a> <a
-                                                            class="btn btn-primary" href="#">&euro;222.00</a></h4>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li class="span3">
-                                            <div class="thumbnail">
-                                                <a href="product_details.html"><img src="themes/images/products/1.jpg"
-                                                        alt="" /></a>
-                                                <div class="caption">
-                                                    <h5>Manicure &amp; Pedicure</h5>
-                                                    <p>
-                                                        Lorem Ipsum is simply dummy text.
-                                                    </p>
-                                                    <h4 style="text-align:center"><a class="btn"
-                                                            href="product_details.html"> <i class="icon-zoom-in"></i></a>
-                                                        <a class="btn" href="#">Add to <i
-                                                                class="icon-shopping-cart"></i></a> <a
-                                                            class="btn btn-primary" href="#">&euro;222.00</a></h4>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li class="span3">
-                                            <div class="thumbnail">
-                                                <a href="product_details.html"><img src="themes/images/products/2.jpg"
-                                                        alt="" /></a>
-                                                <div class="caption">
-                                                    <h5>Manicure &amp; Pedicure</h5>
-                                                    <p>
-                                                        Lorem Ipsum is simply dummy text.
-                                                    </p>
-                                                    <h4 style="text-align:center"><a class="btn"
-                                                            href="product_details.html"> <i class="icon-zoom-in"></i></a>
-                                                        <a class="btn" href="#">Add to <i
-                                                                class="icon-shopping-cart"></i></a> <a
-                                                            class="btn btn-primary" href="#">&euro;222.00</a></h4>
-                                                </div>
-                                            </div>
-                                        </li>
+                                            </li>
+                                        @endforeach
                                     </ul>
                                     <hr class="soft" />
                                 </div>
